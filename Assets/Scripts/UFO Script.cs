991f3791cc028d7abdb4f8c11c1e22295a42b513
jake0822 <jakeyounan16@gmail.com>
2026-01-19 22:21:52 -0500

    Adds ufo movement

--- Assets/Scripts/UFO Script.cs
+++ Assets/Scripts/UFO Script.cs
@@ -1,16 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UFOScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 _input;
+    public void GetMovement(InputAction.CallbackContext context)
     {
-        
+        _input = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        print(_input);
+        Vector3 movement = new Vector3(_input.x, 0f, _input.y);
+        transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
