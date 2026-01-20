d061f59d13e50f430c700648c7ded38fb055fba6
David L Berger <david@broken-pipe.com>
2026-01-20 14:51:05 -0500

    Fixes Collision and Adds SmoothDamp

--- Assets/Scripts/UFO Script.cs
+++ Assets/Scripts/UFO Script.cs
@@ -4,7 +4,10 @@ using UnityEngine.InputSystem;
 
 public class UFOScript : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 4.0f;
+    [SerializeField] private float smoothTime = 2.0f;
+    private Vector3 targetVelocity;
+    private Vector3 velocity = Vector3.zero;
     private Vector2 _input;
     public void GetMovement(InputAction.CallbackContext context)
     {
@@ -14,7 +17,7 @@ public class UFOScript : MonoBehaviour
     private void Update()
     {
         print(_input);
-        Vector3 movement = new Vector3(_input.x, 0f, _input.y);
-        transform.position += movement * moveSpeed * Time.deltaTime;
+        Vector3 movement = (new Vector3(_input.x, 0f, _input.y))*moveSpeed;
+        transform.position = Vector3.SmoothDamp(transform.position, transform.position + movement, ref velocity, smoothTime);
     }
 }
