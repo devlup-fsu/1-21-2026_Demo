07617ac09012c9e30699036f450a9e91d46ee234
jake0822 <jakeyounan16@gmail.com>
2026-01-21 11:34:42 -0500

    Simplify collision

--- Assets/Scripts/killCow.cs
+++ Assets/Scripts/killCow.cs
@@ -7,14 +7,14 @@ public class killCow : MonoBehaviour
 {
    private int cowScore = 0;
    public TextMeshProUGUI text;
-   
-   private void OnTriggerEnter(Collider other)
+
+   private void OnCollisionEnter(Collision other)
    {
-      if (other.CompareTag("cow"))
+      if (other.gameObject.CompareTag("cow"))
       {
          cowScore++;
          print("kill cow");
-         other.GetComponent<MeshRenderer>().enabled = false;
+         other.gameObject.GetComponent<MeshRenderer>().enabled = false;
          text.text = cowScore.ToString();
       }
    }
